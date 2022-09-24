b91028e422b39fff4039d32a304b2454113754c1
Keola Nakamoto <101081243+kn5150@users.noreply.github.com>
2022-09-23 16:40:34 -1000

    Update Making a Field Accept Only Certain Numbers (Error Message Included).js

--- Validation/Making a Field Accept Only Certain Numbers (Error Message Included).js
+++ Validation/Making a Field Accept Only Certain Numbers (Error Message Included).js
@@ -1,8 +1,7 @@
 // This is a custom validation script
 // Numbers that can be inputted
 // TODO: Change all # characters to numbers you would like the field to accept, be it as a range using a dash or as a set of specific options using a comma
-// Note 1: In the curly brackets is the number of digits the value will have, so it's very important to have this updated to match the maximum amount of digits out of the field's options!
-// Note 2: For options with multiple digits, they will need to be wrapped in quotations then separated by a comma. 
+// Note: In the curly brackets is the number of digits the value will have, so it's very important to have this updated to match the maximum amount of digits out of the field's options!
 var re = /([###]{#})/
 // Validate field and execute error message
 if ((re.test(event.value) == false)&&(event.value!=""))
