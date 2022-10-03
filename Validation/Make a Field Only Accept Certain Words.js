c9b3908f3787791836694d22d8a95637eabf53a8
Keola Nakamoto <101081243+kn5150@users.noreply.github.com>
2022-10-03 09:24:54 -1000

    Update Make a Field Only Accept Certain Words.js

--- Validation/Make a Field Only Accept Certain Words.js
+++ Validation/Make a Field Only Accept Certain Words.js
@@ -1,7 +1,7 @@
 // This is a custom validation script
 // TODO: Change "Text1" and "Text2 to the strings that should be accepted
 event.rc = true;
-if (event.value != "" && event.value != "Text1"" && event.value != "Text2")
+if (event.value != "" && event.value != "Text1" && event.value != "Text2")
 {
     app.alert("The entered value needs to be either 'Text1' or 'Text2'!");
     event.rc = false;
