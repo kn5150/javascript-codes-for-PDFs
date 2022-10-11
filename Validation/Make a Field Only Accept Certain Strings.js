67ec8d51d640c0d0e47e1bbf0f0bcada2b65172b
Keola Nakamoto <101081243+kn5150@users.noreply.github.com>
2022-10-10 15:34:54 -1000

    Update Make a Field Only Accept Certain Strings.js

--- Validation/Make a Field Only Accept Certain Strings.js
+++ Validation/Make a Field Only Accept Certain Strings.js
@@ -1,5 +1,6 @@
 // This is a custom validation script
 // TODO: Change "Text1" and "Text2 to the strings that should be accepted
+// Note: This also works with integers
 event.rc = true;
 if (event.value != "" && event.value != "Text1" && event.value != "Text2")
 {
