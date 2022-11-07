713e2f7b4bd94d1096cd13e4aa22986698820a6e
Keola Nakamoto <101081243+kn5150@users.noreply.github.com>
2022-11-07 13:22:55 -1000

    Create State Input.js

---
+++ Validation/State Input.js
@@ -0,0 +1,8 @@
+if (event.value != "") {
+    var r = new RegExp('^(AA|AE|AP|AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY)$');
+    var result = r.test(event.value);
+    if (result == false)  {
+        app.alert("Invalid USPS State Abbreviation");
+        event.rc = false;
+    }
+}
