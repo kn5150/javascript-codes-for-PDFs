28117c8ded344b452b5bd9daf11ce3f930b2a78d
Keola Nakamoto <101081243+kn5150@users.noreply.github.com>
2022-11-14 09:07:07 -1000

    Update State Input.js

--- Validation/State Input.js
+++ Validation/State Input.js
@@ -1,3 +1,7 @@
+// Convert input to uppercase
+event.value = event.value.toUpperCase();
+
+// Validates input against list of USPS abbreviations
 if (event.value != "") {
     var r = new RegExp('^(AA|AE|AP|AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY)$');
     var result = r.test(event.value);
