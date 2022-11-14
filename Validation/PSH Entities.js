787b892bd7a3e3d628fed4be58244440a0327d57
Keola Nakamoto <101081243+kn5150@users.noreply.github.com>
2022-11-14 12:15:19 -1000

    Update PSH Entities.js

--- Validation/PSH Entities.js
+++ Validation/PSH Entities.js
@@ -1,23 +1,45 @@
-if (this.getField("PSH Entity").value = "Proservice Solutions, Inc.") {
-	event.value = ;
-} else if (this.getField("PSH Entity").value = "Proservice Advantage, Inc.") {
-	event.value = ;
-} else if (this.getField("PSH Entity").value = "Proservice Pacific, LLC") {
-	event.value = ;
-} else if (this.getField("PSH Entity").value = "Proservice Elite, Inc.") {
-	event.value = ;
-} else if (this.getField("PSH Entity").value = "Proservice Innovation, Inc.") {
-	event.value = ;
-} else if (this.getField("PSH Entity").value = "Proservice Optimum, Inc.") {
-	event.value = ;
-} else if (this.getField("PSH Entity").value = "Proservice Connect, Inc.") {
-	event.value = ;
-} else if (this.getField("PSH Entity").value = "Proservice Focus, Inc.") {
-	event.value = ;
-} else if (this.getField("PSH Entity").value = "Proservice Xpress, Inc.") {
-	event.value = ;
-} else if (this.getField("PSH Entity").value = "Proservice Human Resources, Inc.") {
-	event.value = ;
+if (event.value == "ProService Solutions, Inc.") {
+	this.getField("FEIN").value = "27-2367902";
+	this.getField("HI WH No").value = "WH-026-680-7296-01";
+	this.getField("DOL No").value = "0007521260";
+} else if (event.value == "ProService Advantage, Inc.") {
+	this.getField("FEIN").value = "45-3140050";	
+	this.getField("HI WH No").value = "WH-028-540-3136-01";
+	this.getField("DOL No").value = "0007570686";
+} else if (event.value == "ProService Pacific, LLC") {
+	this.getField("FEIN").value = "61-1582293";	
+	this.getField("HI WH No").value = "WH-077-588-4800-01";
+	this.getField("DOL No").value = "0007583532";
+} else if (event.value == "ProService Elite, Inc.") {
+	this.getField("FEIN").value = "46-0934477";	
+	this.getField("HI WH No").value = "WH-095-811-1744-01";
+	this.getField("DOL No").value = "0007609850";
+} else if (event.value == "ProService Innovation, Inc.") {
+	this.getField("FEIN").value = "46-1480865";	
+	this.getField("HI WH No").value = "WH-169-933-8240-01";
+	this.getField("DOL No").value = "0007618220";
+} else if (event.value == "ProService Optimum, Inc.") {
+	this.getField("FEIN").value = "46-3198555";	
+	this.getField("HI WH No").value = "WH-002-543-2064-01";
+	this.getField("DOL No").value = "0007644124";
+} else if (event.value == "ProService Connect, Inc.") {
+	this.getField("FEIN").value = "47-2116809";	
+	this.getField("HI WH No").value = "WH-098-704-3840-01";
+	this.getField("DOL No").value = "0007692102";
+} else if (event.value == "ProService Focus, Inc.") {
+	this.getField("FEIN").value = "47-2130690";	
+	this.getField("HI WH No").value = "WH-103-226-7776-01";
+	this.getField("DOL No").value = "0007692242";
+} else if (event.value == "ProService Xpress, Inc.") {
+	this.getField("FEIN").value = "81-3183582";	
+	this.getField("HI WH No").value = "WH-046-915-5840-01";
+	this.getField("DOL No").value = "0007762119";
+} else if (event.value == "ProService Human Resources, Inc.") {
+	this.getField("FEIN").value = "81-3183441";	
+	this.getField("HI WH No").value = "WH-213-433-5488-01";
+	this.getField("DOL No").value = "0007762100";
 } else {
-	event.value = "";
+	this.getField("FEIN").value = "";	
+	this.getField("HI WH No").value = "";
+	this.getField("DOL No").value = "";
 }
