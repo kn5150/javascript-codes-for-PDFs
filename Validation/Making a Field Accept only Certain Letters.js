d8019d874b6928538784f9e835c107af48705353
Keola Nakamoto <101081243+kn5150@users.noreply.github.com>
2022-12-16 17:54:27 -1000

    Delete Making a Field Accept only Certain Letters.js

--- Validation/Making a Field Accept only Certain Letters.js
+++
@@ -1,4 +0,0 @@
-// This is a custom validation script
-// Field only accepts certain letters
-// TODO: Change "@@@" to the letters you want the field to accept
-event.rc = /^[@@@]?$/.test(AFMergeChange(event));
